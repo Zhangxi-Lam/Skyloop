b9d91587b696c1888a1a06876b118c518cf645a8
Eric O. LEBIGOT (EOL) <eric.lebigot@normalesup.org>
2014-09-26 17:03:43 +0800

    First version: documented arguments, behavior, values reading, and file length check.

---
+++ CPU_vs_GPU/compare_cluster_summaries.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+"""
+Compare the skyloop result summary printed for each cluster, between
+two versions of the output.
+"""
+
+import re
+
+VALUES_FINDALL = re.compile("= (\S+)").findall
+
+def get_values(line):
+    """
+    Return the values found after the equal signs in the given line,
+    as floats.
+    """
+    return map(float, VALUES_FINDALL(line))
+    
+if __name__ == "__main__":
+
+    import argparse
+    import itertools
+    import sys
+    
+    parser = argparse.ArgumentParser(
+        description="""
+        Compare the skyloop result summary printed for each cluster,
+        between two versions of the output. The lines are compared one
+        by one and must therefore normally (mostly) match. The values
+        following the equal signs are matched together: the format is
+        '... = <value>' (followed by a space or newline). They should
+        therefore typically correspond to variables printed in the
+        same order. The program checks that the two files have the
+        same number of lines and issues a warning otherwise.""")
+
+    parser.add_argument("path0", help="first output file")
+    parser.add_argument("path1", help="second output file")
+    args = parser.parse_args()
+
+    with open(args.path0) as file0, open(args.path1) as file1:
+        for (line0, line1) in itertools.izip_longest(file0, file1):
+            try:
+                values0, values1 = get_values(line0), get_values(line1)
+            except TypeError:  # Happens if a file is longer: None was parsed
+                print >> sys.stderr, "Warning: one file has more lines"
+                break
+            
+            print values0
+
